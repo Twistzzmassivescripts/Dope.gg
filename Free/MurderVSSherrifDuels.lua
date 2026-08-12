f27dbe62c85b5f4d2ad48b354eb2c9a0663a5e55
Twistzz <frajamir0@gmail.com>
2026-08-12 15:49:52 +0000

    Update MurderVSSherrifDuels.lua

--- Free/MurderVSSherrifDuels.lua
+++ Free/MurderVSSherrifDuels.lua
@@ -1,3 +1,3 @@
-script_key = "%KEY%"
+script_key = "FREE_TRIAL"
 
 loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/2c5d23a178e68b5c60b726f86035f4ef511f13cb6abc0dcae76b72bf179d22b8.lua"))()
